77402584554d78fc1afcb6d7dbe9d8137e908747
yuxuanli <yuxuanli@v2.cluster.local>
2024-10-15 16:06:53 -0400

    initial commit

---
+++ test_vec_inf.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+# The url is located in the .vLLM_model-variant_url file in the corresponding model directory.
+client = OpenAI(base_url="http://gpu067:8080/v1", api_key="EMPTY")
+
+# Update the model path accordingly
+completion = client.chat.completions.create(
+    model="Meta-Llama-3.1-8B-Instruct",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a pirate chatbot who always responds in pirate speak!",
+        },
+        {"role": "user", "content": "Who are you?"},
+    ],
+)
+
+print(completion)
